599adbe22cedc57217d49e7b54dc02e33c418292
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2021-03-31 10:29:25 +0200

    explicit return for initialization functions rather than in-place

--- julia/miniWeather_serial.jl
+++ julia/miniWeather_serial.jl
@@ -175,13 +175,20 @@ function init(config)
                     z = (k_beg + k-hs+0.5)*dz + (qpoints[kk]-0.5)*dz
 
                     #Set the fluid state based on the user's specification
-                    if data_spec_int == Int(DATA_SPEC_COLLISION)       collision!(x,z,r,u,w,t,hr,ht)       end
-                    if data_spec_int == Int(DATA_SPEC_THERMAL)         thermal!(x,z,r,u,w,t,hr,ht)         end
-                    if data_spec_int == Int(DATA_SPEC_MOUNTAIN)        mountain_waves!(x,z,r,u,w,t,hr,ht)  end
-                    if data_spec_int == Int(DATA_SPEC_TURBULENCE)      turbulence!(x,z,r,u,w,t,hr,ht)      end
-                    if data_spec_int == Int(DATA_SPEC_DENSITY_CURRENT) density_current!(x,z,r,u,w,t,hr,ht) end
-                    if data_spec_int == Int(DATA_SPEC_INJECTION)       injection!(x,z,r,u,w,t,hr,ht)       end
-
+                    if data_spec_int == Int(DATA_SPEC_COLLISION)      
+                        r, t, u, w, hr, ht = collision(x,z)
+                    elseif data_spec_int == Int(DATA_SPEC_THERMAL)
+                        r, t, u, w, hr, ht = thermal(x,z)  
+                    elseif data_spec_int == Int(DATA_SPEC_MOUNTAIN)
+                        r, t, u, w, hr, ht = mountain_waves(x,z) 
+                    elseif data_spec_int == Int(DATA_SPEC_TURBULENCE)     
+                        r, t, u, w, hr, ht = turbulence(x,z) 
+                    elseif data_spec_int == Int(DATA_SPEC_DENSITY_CURRENT) 
+                        r, t, u, w, hr, ht = density_current(x,z) 
+                    elseif data_spec_int == Int(DATA_SPEC_INJECTION)      
+                        r, t, u, w, hr, ht = injection(x,z) 
+                    end            
+                    #println(r, hr, u, w, ht)
                     #Store into the fluid state array
                     model.state[ID_DENS,k,i] += r                         * qweights[ii]*qweights[kk]
                     model.state[ID_UMOM,k,i] += (r+hr)*u                  * qweights[ii]*qweights[kk]
@@ -189,28 +196,35 @@ function init(config)
                     model.state[ID_RHOT,k,i] += ( (r+hr)*(t+ht) - hr*ht ) * qweights[ii]*qweights[kk]
                 end
             end
-        for ll=1:NUM_VARS
-            model.state_tmp[ll,k,i] = model.state[ll,k,i]
+            for ll=1:NUM_VARS
+                model.state_tmp[ll,k,i] = model.state[ll,k,i]
+            end
         end
     end
-end
-  #Compute the hydrostatic background state over vertical cell averages
-  ########################/
-  # TODO: MAKE THIS LOOP A PARALLEL_FOR
-  ########################/
-  for k=1:nz+2*hs
-      for kk=1:nqpoints
-          z = (k_beg + k-hs+0.5)*dz
-          #Set the fluid state based on the user's specification
-          if data_spec_int == Int(DATA_SPEC_COLLISION)       collision!(0.,z,r,u,w,t,hr,ht) end
-          if data_spec_int == Int(DATA_SPEC_THERMAL)         thermal!(0.,z,r,u,w,t,hr,ht)  end
-          if data_spec_int == Int(DATA_SPEC_MOUNTAIN)        mountain_waves!(0.,z,r,u,w,t,hr,ht) end 
-          if data_spec_int == Int(DATA_SPEC_TURBULENCE)      turbulence!(0.,z,r,u,w,t,hr,ht) end
-          if data_spec_int == Int(DATA_SPEC_DENSITY_CURRENT) density_current!(0.,z,r,u,w,t,hr,ht) end
-          if data_spec_int == Int(DATA_SPEC_INJECTION)       injection!(0.,z,r,u,w,t,hr,ht) end
-          model.hy_dens_cell[k] = model.hy_dens_cell[k] + hr    * qweights[kk] 
-          model.hy_dens_theta_cell[k] = model.hy_dens_theta_cell[k] + hr*ht * qweights[kk]
-      end
+    #Compute the hydrostatic background state over vertical cell averages
+    ########################/
+    # TODO: MAKE THIS LOOP A PARALLEL_FOR
+    ########################/
+    for k=1:nz+2*hs
+        for kk=1:nqpoints
+            z = (k_beg + k-hs+0.5)*dz
+            #Set the fluid state based on the user's specification
+            if data_spec_int == Int(DATA_SPEC_COLLISION)      
+                r, t, u, w, hr, ht = collision(0.,z)
+            elseif data_spec_int == Int(DATA_SPEC_THERMAL)
+                r, t, u, w, hr, ht = thermal(0.,z)  
+            elseif data_spec_int == Int(DATA_SPEC_MOUNTAIN)
+                r, t, u, w, hr, ht = mountain_waves(0.,z) 
+            elseif data_spec_int == Int(DATA_SPEC_TURBULENCE)     
+                r, t, u, w, hr, ht = turbulence(0.,z) 
+            elseif data_spec_int == Int(DATA_SPEC_DENSITY_CURRENT) 
+                r, t, u, w, hr, ht = density_current(0.,z) 
+            elseif data_spec_int == Int(DATA_SPEC_INJECTION)      
+                r, t, u, w, hr, ht = injection(0.,z) 
+            end
+            model.hy_dens_cell[k] = model.hy_dens_cell[k] + hr    * qweights[kk] 
+            model.hy_dens_theta_cell[k] = model.hy_dens_theta_cell[k] + hr*ht * qweights[kk]
+        end
     end
     #Compute the hydrostatic background state at vertical cell interfaces
     ########################/
@@ -218,31 +232,39 @@ end
     ########################/
     for k=1:nz+1
         z = (k_beg + k)*dz
-        if data_spec_int == Int(DATA_SPEC_COLLISION)      collision!(0.,z,r,u,w,t,hr,ht) end
-        if data_spec_int == Int(DATA_SPEC_THERMAL)        thermal!(0.,z,r,u,w,t,hr,ht);  end
-        if data_spec_int == Int(DATA_SPEC_MOUNTAIN)       mountain_waves!(0.,z,r,u,w,t,hr,ht) end 
-        if data_spec_int == Int(DATA_SPEC_TURBULENCE)     turbulence!(0.,z,r,u,w,t,hr,ht) end
-        if data_spec_int == Int(DATA_SPEC_DENSITY_CURRENT) density_current!(0.,z,r,u,w,t,hr,ht) end
-        if data_spec_int == Int(DATA_SPEC_INJECTION)      injection!(0.,z,r,u,w,t,hr,ht) end
+        if data_spec_int == Int(DATA_SPEC_COLLISION)      
+            r, t, u, w, hr, ht = collision(0.,z)
+        elseif data_spec_int == Int(DATA_SPEC_THERMAL)
+            r, t, u, w, hr, ht = thermal(0.,z)  
+        elseif data_spec_int == Int(DATA_SPEC_MOUNTAIN)
+            r, t, u, w, hr, ht = mountain_waves(0.,z) 
+        elseif data_spec_int == Int(DATA_SPEC_TURBULENCE)     
+            r, t, u, w, hr, ht = turbulence(0.,z) 
+        elseif data_spec_int == Int(DATA_SPEC_DENSITY_CURRENT) 
+            r, t, u, w, hr, ht = density_current(0.,z) 
+        elseif data_spec_int == Int(DATA_SPEC_INJECTION)      
+            r, t, u, w, hr, ht = injection(0.,z) 
+        end
         model.hy_dens_int[k] = hr
         model.hy_dens_theta_int[k] = hr * ht
-        model.hy_pressure_int[k] = C0 * (hr*ht)^gamm
+        model.hy_pressure_int[k] = C0 * (hr*ht)^gamma
     end
-
-    return model
-end
+  
+    return model, grid
+end 
 
 
 #This test case is initially balanced but injects fast, cold air from the left boundary near the model top
 #x and z are input coordinates at which to sample
 #r,u,w,t are output density, u-wind, w-wind, and potential temperature at that location
 #hr and ht are output background hydrostatic density and potential temperature at that location
-function injection!(x , z , r , u , w , t , hr , ht) 
-    hydro_const_theta!(z,hr,ht)
+function injection(x , z) 
+    hydro_const_theta(z,hr,ht)
     r = 0.
     t = 0.
     u = 0.
     w = 0.
+    return r, t, u, w, hr, ht
 end
 
 
@@ -250,21 +272,22 @@ end
 #x and z are input coordinates at which to sample
 #r,u,w,t are output density, u-wind, w-wind, and potential temperature at that location
 #hr and ht are output background hydrostatic density and potential temperature at that location
-function density_current!(x, z , r , u , w , t , hr , ht) 
-    hydro_const_theta!(z,hr,ht)
+function density_current(x, z) 
+    hr, ht = hydro_const_theta(z)
     r = 0.
     t = 0.
     u = 0.
     w = 0.
-    t = t + sample_ellipse_cosine!(x,z,-20. ,xlen/2,5000.,4000.,2000.)
+    t = t + sample_ellipse_cosine(x,z,-20. ,xlen/2,5000.,4000.,2000.)
+    return r, t, u, w, hr, ht
 end
 
 
 #x and z are input coordinates at which to sample
 #r,u,w,t are output density, u-wind, w-wind, and potential temperature at that location
 #hr and ht are output background hydrostatic density and potential temperature at that location
-function turbulence!(x , z, r, u, w, t, hr, ht)
-    hydro_const_theta!(z,hr,ht)
+function turbulence(x , z)
+    hr, ht = hydro_const_theta(z)
     r = 0.
     t = 0.
     u = 0.
@@ -273,18 +296,20 @@ function turbulence!(x , z, r, u, w, t, hr, ht)
     # call random_number(w);
     # u = (u-0.5)*20;
     # w = (w-0.5)*20;
+    return r, t, u, w, hr, ht
 end
 
 
 #x and z are input coordinates at which to sample
 #r,u,w,t are output density, u-wind, w-wind, and potential temperature at that location
 #hr and ht are output background hydrostatic density and potential temperature at that location
-function mountain_waves!(x, z, r, u, w, t, hr, ht)
-    hydro_const_bvfreq!(z,0.02,hr,ht)
+function mountain_waves(x, z)
+    hr, ht = hydro_const_bvfreq(z, 0.02)
     r = 0.
     t = 0.
     u = 15.
     w = 0.
+    return r, t, u, w, hr, ht
 end
 
 
@@ -292,13 +317,15 @@ end
 #x and z are input coordinates at which to sample
 #r,u,w,t are output density, u-wind, w-wind, and potential temperature at that location
 #hr and ht are output background hydrostatic density and potential temperature at that location
-function thermal!(x, z, r, u, w, t, hr, ht)
-    hydro_const_theta!(z,hr,ht)
+function thermal(x, z)
+    hr, ht = hydro_const_theta(z)
     r = 0.
     t = 0.
     u = 0.
     w = 0.
-    t = t + sample_ellipse_cosine!(x,z, 3. ,xlen/2,2000.,2000.,2000.)
+    t = t + sample_ellipse_cosine(x,z, 3. ,xlen/2,2000.,2000.,2000.)
+    #println(t)
+    return r, t, u, w, hr, ht
 end
 
 
@@ -307,49 +334,51 @@ end
 #r,u,w,t are output density, u-wind, w-wind, and potential temperature at that location
 #hr and ht are output background hydrostatic density and potential temperature at that location
 function collision!(x, z, r, u, w, t, hr, ht)
-    hydro_const_theta!(z,hr,ht)
+    hr, ht = hydro_const_theta!(z)
     r = 0.
     t = 0.
     u = 0.
     w = 0.
-    t = t + sample_ellipse_cosine!(x,z, 20.,xlen/2,2000.,2000.,2000.)
-    t = t + sample_ellipse_cosine!(x,z,-20.,xlen/2,8000.,2000.,2000.)
+    t = t + sample_ellipse_cosine(x,z, 20.,xlen/2,2000.,2000.,2000.)
+    t = t + sample_ellipse_cosine(x,z,-20.,xlen/2,8000.,2000.,2000.)
 end
 
 
-#Establish hydrstatic balance using constant potential temperature (thermally neutral atmosphere)
+#Establish hydrostatic balance using constant potential temperature (thermally neutral atmosphere)
 #z is the input coordinate
 #r and t are the output background hydrostatic density and potential temperature
-function hydro_const_theta!(z, r, t)
-  theta0 = 300.  #Background potential temperature
-  exner0 = 1.    #Surface-level Exner pressure
-  #Establish hydrostatic balance first using Exner pressure
-  t = theta0                                  #Potential Temperature at z
-  exner = exner0 - grav * z / (cp * theta0)   #Exner pressure at z
-  p = p0 * exner^(cp/rd)                 #Pressure at z
-  rt = (p / C0)^(1. / gamm)              #rho*theta at z
-  r = rt / t                                  #Density at z
+function hydro_const_theta(z)
+    theta0 = 300.  #Background potential temperature
+    exner0 = 1.    #Surface-level Exner pressure
+    #Establish hydrostatic balance first using Exner pressure
+    ht = theta0                                  #Potential Temperature at z
+    exner = exner0 - grav * z / (cp * theta0)   #Exner pressure at z
+    p = p0 * exner^(cp/rd)                 #Pressure at z
+    rt = (p / C0)^(1. / gamma)              #rho*theta at z
+    hr = rt / ht                                  #Density at z
+    return hr, ht
 end
 
 #Establish hydrstatic balance using constant Brunt-Vaisala frequency
 #z is the input coordinate
 #bv_freq0 is the constant Brunt-Vaisala frequency
 #r and t are the output background hydrostatic density and potential temperature
-function hydro_const_bvfreq!(z, bv_freq0, r, t)
+function hydro_const_bvfreq(z, bv_freq0)
     theta0 = 300.  #Background potential temperature
     exner0 = 1.    #Surface-level Exner pressure
-    t = theta0 * exp( bv_freq0*bv_freq0 / grav * z )                                    #Pot temp at z
-    exner = exner0 - grav*grav / (cp * bv_freq0*bv_freq0) * (t - theta0) / (t * theta0) #Exner pressure at z
+    ht = theta0 * exp(bv_freq0*bv_freq0 / grav * z)                                    #Pot temp at z
+    exner = exner0 - grav*grav / (cp * bv_freq0*bv_freq0) * (ht - theta0) / (ht * theta0) #Exner pressure at z
     p = p0 * exner^(cp/rd)                                                         #Pressure at z
-    rt = (p / C0)^(1. / gamm)                                                  #rho*theta at z
-    r = rt / t                                                                          #Density at z
+    rt = (p / C0)^(1. / gamma)                                                  #rho*theta at z
+    hr = rt / ht      
+    return hr, ht                                                                    #Density at z
 end
 
 
 #Sample from an ellipse of a specified center, radius, and amplitude at a specified location
 #x and z are input coordinates
 #amp,x0,z0,xrad,zrad are input amplitude, center, and radius of the ellipse
-function sample_ellipse_cosine!(x, z, amp, x0, z0, xrad, zrad)
+function sample_ellipse_cosine(x, z, amp, x0, z0, xrad, zrad)
     #Compute distance from bubble center
     dist = sqrt( ((x-x0)/xrad)*((x-x0)/xrad) + ((z-z0)/zrad)*((z-z0)/zrad) ) * pi / 2.
     #If the distance from bubble center is less than the radius, create a cos**2 profile
