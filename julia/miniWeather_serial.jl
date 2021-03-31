5b5fd2991d4e31bef7a4c9d8f84dfd3f5ba7b516
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2021-03-31 14:47:18 +0200

    WIP writing netcdf-output function

--- julia/miniWeather_serial.jl
+++ julia/miniWeather_serial.jl
@@ -62,19 +62,16 @@ function reductions(model::Model, grid::Grid)
 end
 
 
-#= 
-
 #Output the fluid state (state) to a NetCDF file at a given elapsed model time (etime)
 #The file I/O uses parallel-netcdf, the only external library required for this mini-app.
 #If it's too cumbersome, you can comment the I/O out, but you'll miss out on some potentially cool graphics
-function output(state, etime, grid)
+function output(state, etime, grid, ncfile="output.nc")
   using NetCDF
   integer :: ncid, t_dimid, x_dimid, z_dimid, dens_varid, uwnd_varid, wwnd_varid, theta_varid, t_varid
   integer :: i,k
   integer, save :: num_out = 0
   integer(kind=MPI_OFFSET_KIND) :: len, st1(1),ct1(1),st3(3),ct3(3)
-  !Temporary arrays to hold density, u-wind, w-wind, and potential temperature (theta)
-  real(rp), allocatable :: dens(:,:), uwnd(:,:), wwnd(:,:), theta(:,:)
+  #Temporary arrays to hold density, u-wind, w-wind, and potential temperature (theta)
   real(rp) :: etimearr(1)
   #Allocate some (big) temp arrays
   dens = zeros(grid.nx, grid.nz)
@@ -82,63 +79,42 @@ function output(state, etime, grid)
   wwnd = zeros(grid.nx, grid.nz)
   theta = zeros(grid.nx,grid.nz)
 
-  #If the elapsed time is zero, create the file. Otherwise, open the file
-  if etime == 0 
-    !Create the file
-    nccreate("output.nc", varname, "t", collect(11:20), "t", 20, Dict("units"=>"s"), atts=attribs)
-
-    call ncwrap( nf90mpi_create( MPI_COMM_WORLD , 'output.nc' , nf90_clobber , MPI_INFO_NULL , ncid ) , __LINE__ )
-    !Create the dimensions
-    len=nf90_unlimited; call ncwrap( nfmpi_def_dim( ncid , 't' , len , t_dimid ) , __LINE__ )
-    len=nx_glob       ; call ncwrap( nfmpi_def_dim( ncid , 'x' , len , x_dimid ) , __LINE__ )
-    len=nz_glob       ; call ncwrap( nfmpi_def_dim( ncid , 'z' , len , z_dimid ) , __LINE__ )
-    !Create the variables
-    call ncwrap( nfmpi_def_var( ncid , 't' , nf90_double , 1 , (/ t_dimid /) , t_varid ) , __LINE__ )
-    call ncwrap( nfmpi_def_var( ncid , 'dens'  , nf90_double , 3 , (/ x_dimid , z_dimid , t_dimid /) ,  dens_varid ) , __LINE__ )
-    call ncwrap( nfmpi_def_var( ncid , 'uwnd'  , nf90_double , 3 , (/ x_dimid , z_dimid , t_dimid /) ,  uwnd_varid ) , __LINE__ )
-    call ncwrap( nfmpi_def_var( ncid , 'wwnd'  , nf90_double , 3 , (/ x_dimid , z_dimid , t_dimid /) ,  wwnd_varid ) , __LINE__ )
-    call ncwrap( nfmpi_def_var( ncid , 'theta' , nf90_double , 3 , (/ x_dimid , z_dimid , t_dimid /) , theta_varid ) , __LINE__ )
-    !End "define" mode
-    call ncwrap( nfmpi_enddef( ncid ) , __LINE__ )
-  else
-    !Open the file
-    call ncwrap( nfmpi_open( MPI_COMM_WORLD , 'output.nc' , nf90_write , MPI_INFO_NULL , ncid ) , __LINE__ )
-    !Get the variable IDs
-    call ncwrap( nfmpi_inq_varid( ncid , 'dens'  ,  dens_varid ) , __LINE__ )
-    call ncwrap( nfmpi_inq_varid( ncid , 'uwnd'  ,  uwnd_varid ) , __LINE__ )
-    call ncwrap( nfmpi_inq_varid( ncid , 'wwnd'  ,  wwnd_varid ) , __LINE__ )
-    call ncwrap( nfmpi_inq_varid( ncid , 'theta' , theta_varid ) , __LINE__ )
-    call ncwrap( nfmpi_inq_varid( ncid , 't'     ,     t_varid ) , __LINE__ )
-  endif
-
-  !Store perturbed values in the temp arrays for output
+  #Store perturbed values in the temp arrays for output
   do k = 1 , nz
     do i = 1 , nx
-      dens (i,k) = state(i,k,ID_DENS)
-      uwnd (i,k) = state(i,k,ID_UMOM) / ( hy_dens_cell(k) + state(i,k,ID_DENS) )
-      wwnd (i,k) = state(i,k,ID_WMOM) / ( hy_dens_cell(k) + state(i,k,ID_DENS) )
-      theta(i,k) = ( state(i,k,ID_RHOT) + hy_dens_theta_cell(k) ) / ( hy_dens_cell(k) + state(i,k,ID_DENS) ) - hy_dens_theta_cell(k) / hy_dens_cell(k)
-    enddo
-  enddo
+      dens[i,k] = state[i,k,ID_DENS]
+      uwnd[i,k] = state[i,k,ID_UMOM] / (hy_dens_cell[k] + state[i,k,ID_DENS])
+      wwnd[i,k] = state[i,k,ID_WMOM] / (hy_dens_cell[k] + state[i,k,ID_DENS])
+      theta[i,k] = (state[,k,ID_RHOT] + hy_dens_theta_cell[k]) / (hy_dens_cell[k] + state[i,k,ID_DENS]) - hy_dens_theta_cell[k] / hy_dens_cell[k]
+    end
+  end
 
-  !Write the grid data to file with all the processes writing collectively
-  st3=(/i_beg,k_beg,num_out+1/); ct3=(/nx,nz,1/); call ncwrap( nfmpi_put_vara_double_all( ncid ,  dens_varid , st3 , ct3 , dens  ) , __LINE__ )
-  st3=(/i_beg,k_beg,num_out+1/); ct3=(/nx,nz,1/); call ncwrap( nfmpi_put_vara_double_all( ncid ,  uwnd_varid , st3 , ct3 , uwnd  ) , __LINE__ )
-  st3=(/i_beg,k_beg,num_out+1/); ct3=(/nx,nz,1/); call ncwrap( nfmpi_put_vara_double_all( ncid ,  wwnd_varid , st3 , ct3 , wwnd  ) , __LINE__ )
-  st3=(/i_beg,k_beg,num_out+1/); ct3=(/nx,nz,1/); call ncwrap( nfmpi_put_vara_double_all( ncid , theta_varid , st3 , ct3 , theta ) , __LINE__ )
+  #If the elapsed time is zero, create the file. Otherwise, open the file
+  if etime == 0 
+    #Create the file
+    nccreate(ncfile, "t", "time", 1, Dict("units"=>"s"))
+    nccreate(ncfile, "dens", "x", nx, Dict("units"=>"m"), "z", nz, Dict("units"=>"m"), "time", 1, Dict("units"=>"s"))
+    nccreate(ncfile, "uwnd", "x", nx, Dict("units"=>"m"), "z", nz, Dict("units"=>"m"), "time", 1, Dict("units"=>"s"))
+    nccreate(ncfile, "wwnd", "x", nx, Dict("units"=>"m"), "z", nz, Dict("units"=>"m"), "time", 1, Dict("units"=>"s"))
+    nccreate(ncfile, "theta", "x", nx, Dict("units"=>"m"), "z", nz, Dict("units"=>"m"), "time", 1, Dict("units"=>"s"))    
+    #Write the grid data to file with all the processes writing collectively
+    ncwrite(dens, ncfile, "dens")
+    ncwrite(uwnd, ncfile, "uwnd")
+    ncwrite(wwnd, ncfile, "wwnd")
+    ncwrite(theta, ncfile, "theta")
+    ncwrite(etime, ncfile, "time")
 
-  !Only the master process needs to write the elapsed time
-  !Begin "independent" write mode
-  call ncwrap( nfmpi_begin_indep_data(ncid) , __LINE__ )
-  !write elapsed time to file
-  if (masterproc) then
-    st1=(/num_out+1/); ct1=(/1/); etimearr(1) = etime; call ncwrap( nfmpi_put_vara_double( ncid , t_varid , st1 , ct1 , etimearr ) , __LINE__ )
-  endif
-  !End "independent" write mode
-  call ncwrap( nfmpi_end_indep_data(ncid) , __LINE__ )
+end
 
-  !Close the file
-  call ncwrap( nf90mpi_close(ncid) , __LINE__ )
+
+  #Write the grid data to file with all the processes writing collectively
+  ncwrite(dens, ncfile, "dens")
+  ncwrite(uwnd, ncfile, "uwnd")
+  ncwrite(wwnd, ncfile, "wwnd")
+  ncwrite(theta, ncfile, "theta")
+  ncwrite(etime, ncfile, "time")
+
+
 
   !Increment the number of outputs
   num_out = num_out + 1
@@ -149,7 +125,7 @@ function output(state, etime, grid)
   deallocate(wwnd )
   deallocate(theta)
 end 
- =#
+ 
 
 
 
